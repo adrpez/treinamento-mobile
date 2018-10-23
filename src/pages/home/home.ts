ee9ed44cb2eb7d52b1b961d6f7db5edf838cb3d1
“Arthur <arthur.santos@ntconsult.com.br>
2018-10-22 23:38:37 -0300

    home layout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Nav } from 'ionic-angular';
+import { EstoquePage } from '../estoque/estoque';
+import { ProdutosPage } from '../produtos/produtos';
+import { VendasPage } from '../vendas/vendas';
+import { ComprasPage } from '../compras/compras';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the HomePage page.
@@ -13,12 +18,30 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = EstoquePage;
+  pages: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.pages = [
+      { title: 'Estoque', component: HomePage },
+      { title: 'Produtos', component: ProdutosPage },
+      { title: 'Compras', component: ComprasPage },
+      { title: 'Vendas', component: VendasPage }
+    ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+
+  sair() {
+    this.nav.setRoot(LoginPage);
   }
 
 }
